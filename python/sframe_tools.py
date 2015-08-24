2da79d6f31fea93700d06fc910d734b2087ed313
Dominik Nowatschin <dominik.nowatschin@desy.de>
2015-08-24 16:39:02 +0200

    From GitTagger: Next Test of Gittagger

--- python/sframe_tools.py
+++ python/sframe_tools.py
@@ -114,13 +114,13 @@ def mk_sframe_and_plot_tools_loose():
         xml_tree_callback=set_eventnumber_and_datasets(count="100", allowed_datasets=tptp_loose_datasets), # 
     )
     plots = varial.tools.ToolChainParallel(
-        'Plots3',
+        'Plots4',
         lazy_eval_tools_func=lambda: plot_loose.mk_tools()
     )
     tc = varial.tools.ToolChain(
         "EventLooptAndPlots",
         [
-            sframe,
+            # sframe,
             plots
         ]
     )
