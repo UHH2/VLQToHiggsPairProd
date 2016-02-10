84a8d788cccf1f8b5585077f105c53b1977acfb5
Dominik Nowatschin <dominik.nowatschin@desy.de>
2016-02-10 14:04:56 +0100

    In TpTpFinalSelectionTreeOutput-v7: redid Ak8 jet lepton cleaning (with dR<0.8 parameter)

--- python/plot.py
+++ python/plot.py
@@ -204,8 +204,8 @@ def loader_hook_finalstates_excl(wrps):
     wrps = gen.sort(wrps, ['in_file_path', 'sample'])
     wrps = list(wrps)
     # for w in wrps: print w.sample, w.in_file_path 
-    # wrps = vlq_common.merge_decay_channels(wrps, ['_thth', '_thtz', '_thbw'], suffix='_thX', print_warning=False)
-    # wrps = vlq_common.merge_decay_channels(wrps, ['_noH_tztz', '_noH_tzbw', '_noH_bwbw'], suffix='_other', print_warning=False)
+    wrps = vlq_common.merge_decay_channels(wrps, ['_thth', '_thtz', '_thbw'], suffix='_thX', print_warning=False)
+    wrps = vlq_common.merge_decay_channels(wrps, ['_noH_tztz', '_noH_tzbw', '_noH_bwbw'], suffix='_other', print_warning=False)
     wrps = gen.sort(wrps, ['in_file_path'])
     return wrps
 
